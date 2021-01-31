91322a6ebe709d27bd4e75b82c329f24e0cfe8ed
Semaj Primm <semajthomasprimm@users.noreply.github.com>
2021-01-30 21:18:31 -0500

    Added semicolon to db queries

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post("/todos", async(req, res) => {
     try{
         const { description } = req.body;
         const newTodo = await pool.query(
-            "INSERT INTO todo (description) VALUES($1) RETURNING *",
+            "INSERT INTO todo (description) VALUES($1) RETURNING *;",
             [description]
         );
         res.json(newTodo.rows[0]);
@@ -43,7 +43,7 @@ app.post("/todos", async(req, res) => {
 // Get all todos
 app.get("/todos", async(req, res) =>{
     try{
-        const allTodos = await pool.query("SELECT * FROM todo");
+        const allTodos = await pool.query("SELECT * FROM todo;");
         res.json(allTodos.rows);
     } catch(err){
         console.error(err.message);
@@ -55,7 +55,7 @@ app.get("/todos/:id", async(req, res) =>{
     try{
         const { id } = req.params;
         const todo = await pool.query(
-            "SELECT * FROM todo WHERE todo_id = $1", 
+            "SELECT * FROM todo WHERE todo_id = $1;", 
             [id]
         );
         res.json(todo.rows[0]);
@@ -71,7 +71,7 @@ app.put("/todos/:id", async(req, res) =>{
         const {id} = req.params;
         const {description} = req.body;
         const updateTodo = await pool.query(
-            "UPDATE todo SET description = $1 WHERE todo_id = $2", 
+            "UPDATE todo SET description = $1 WHERE todo_id = $2;", 
             [description, id]
         );
         res.json("Todo was updated!");
@@ -86,7 +86,7 @@ app.delete("/todos/:id", async(req, res) =>{
     try {
         const { id } = req.params;
         const deleteTodo = await pool.query(
-            "DELETE FROM todo WHERE todo_id = $1",
+            "DELETE FROM todo WHERE todo_id = $1;",
             [id]
         );
         res.json("Todo was deleted");
